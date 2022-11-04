6a415b4ee2cbd5ebaaf83055c2f46f5a6045827c
rborkowski <rafal.borkowski@cyberuslabs.com>
2022-11-04 11:56:31 -0400

    configured docker compose and checks to wait for db

--- app/app/calc.py
+++ app/app/calc.py
@@ -3,10 +3,12 @@ Calculator function
 """
 from typing import Union
 
+
 def add(x: Union[int, float], y: Union[int, float]):
-  """Add two numbers together"""
-  return x + y
+    """Add two numbers together"""
+    return x + y
+
 
 def subtract(x: int, y: int) -> int:
-  """Subtract two numbers together"""
-  return y - x
+    """Subtract two numbers together"""
+    return y - x
