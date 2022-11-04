15805662ee0f5351409cfc05ccbf51ba6fce1765
rborkowski <rafal.borkowski@cyberuslabs.com>
2022-11-04 10:11:24 -0400

    Added database configuration

---
+++ app/app/calc.py
@@ -0,0 +1,12 @@
+"""
+Calculator function
+"""
+from typing import Union
+
+def add(x: Union[int, float], y: Union[int, float]):
+  """Add two numbers together"""
+  return x + y
+
+def subtract(x: int, y: int) -> int:
+  """Subtract two numbers together"""
+  return y - x
